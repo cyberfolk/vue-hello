e522f40a1925a7dd9b1b11cc7677f992253cc83f
cyberfolk <andrea1995ferrari.com>
2023-03-28 15:26:49 +0200

    Initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+/**
+ * CONSEGNA:
+ * Stampare a schermo un messaggio all’interno di un h1 utilizzando una proprietá nell'oggetto restituito dalla funzione data.
+ * 
+ * BONUS:
+ * Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+ */
